9643a4e0883c53c05fb6a4281129dcecdb25dbd0
Theo <acwazytomato@gmail.com>
2015-11-02 23:28:34 -0800

    take out onload

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,11 +26,11 @@
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
 
-			$window.on('load', function() {
+			// $window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
 				}, 100);
-			});
+			// });
 
 		// Mobile?
 			if (skel.vars.mobile)
